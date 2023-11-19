f72d38c7a1d9753de7b3394b79a0cad3a06c6c33
William Ray <will513r@gmail.com>
2023-11-19 13:55:43 -0500

    building clock

---
+++ main.js
@@ -0,0 +1,13 @@
+var now = new Date();
+var hours = now.getHours();
+var mins = now.getMinutes();
+var secs = now.getSeconds();
+var ampm = now.toLocaleTimeString('en-US', {hour12: true});
+
+
+
+
+
+const formattedTime = hours+':'+mins+':'+secs+' '+ampm;
+
+document.getElementById('clock').innerHTML = formattedTime;
