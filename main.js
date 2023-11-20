a1d122a21852ca3ce1a7952e3793fdadb7dbe8d9
Will <will513r@gmail.com>
2023-11-20 11:33:40 -0500

    added quotes

--- main.js
+++ main.js
@@ -15,3 +15,20 @@ document.getElementById("clock").innerHTML = formattedTime;
 
 setInterval(updateClock, 1000);
 updateClock();
+
+
+  async function fetchQuote() {
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      const { content, author } = data;
+
+      document.getElementById("quote").textContent = content;
+      document.getElementById("author").textContent = `- ${author}`;
+    } catch (error) {
+      console.log("Error fetching quote", error);
+    }
+  }
+
+  // Fetch a quote on page load
+  fetchQuote();
