1ee9043b84146dc8c00117c27c4c03ffd96da065
Will <will513r@gmail.com>
2023-11-20 11:11:16 -0500

    Update main.js
    Found a simpler way to get clock details without manually getting each section  and found out why was displaying two clocks.

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 function updateClock(){
-var now = new Date();
-var hours = now.getHours();
-var mins = now.getMinutes();
-var secs = now.getSeconds();
-var ampm = now.toLocaleTimeString('en-US', {hour12: true});
 
-const formattedTime = hours + ":" + mins + ":" + secs + " " + ampm;
+
+ var now = new Date();
+ var formattedTime = now.toLocaleTimeString("en-US", { hour12: true });
 
 document.getElementById("clock").innerHTML = formattedTime;
 
 
 
 }
+
+
+
+
 setInterval(updateClock, 1000);
 updateClock();
