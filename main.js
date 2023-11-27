00e1552b423a6820c0ecc0f99bc0e985b30e6cbf
Will <will513r@gmail.com>
2023-11-27 14:52:34 -0500

    I dont even know anymore

--- main.js
+++ main.js
@@ -1,34 +1,27 @@
-function updateClock(){
+function updateClock() {
+  var now = new Date();
+  var formattedTime = now.toLocaleTimeString("en-US", { hour12: true });
 
-
- var now = new Date();
- var formattedTime = now.toLocaleTimeString("en-US", { hour12: true });
-
-document.getElementById("clock").innerHTML = formattedTime;
-
-
-
+  document.getElementById("clock").innerHTML = formattedTime;
 }
-
-
-
 
 setInterval(updateClock, 1000);
 updateClock();
 
+async function fetchQuote() {
+  try {
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    const { content, author } = data;
 
-  async function fetchQuote() {
-    try {
-      const response = await fetch("https://api.quotable.io/random");
-      const data = await response.json();
-      const { content, author } = data;
-
-      document.getElementById("quote").textContent = content;
-      document.getElementById("author").textContent = `- ${author}`;
-    } catch (error) {
-      console.log("Error fetching quote", error);
-    }
+    document.getElementById("quote").textContent = content;
+    document.getElementById("author").textContent = `- ${author}`;
+  } catch (error) {
+    console.log("Error fetching quote", error);
   }
+}
 
-  // Fetch a quote on page load
-  fetchQuote();
+// Fetch a quote on page load
+fetchQuote();
+getRandomImage();
+// getWeather();
---
+++ randomImage.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const apiKey = process.env.API_KEY;
+
+function displayImage(data) {
+  const imageElement = document.createElement("img");
+  imageElement.src = data.urls.regular;
+  imageElement.alt = data.alt_description || "Random Image";
+
+  const imageContainer = document.getElementById("random-image");
+  imageContainer.innerHTML = ""; 
+  imageContainer.appendChild(imageElement);
+}
+
+async function getRandomImage() {
+  try {
+    const response = await fetch("https://api.unsplash.com/photos/random", {
+      headers: {
+        Authorization: `Client-ID ${apiKey}`,
+      },
+    });
+    const data = await response.json();
+    displayImage(data);
+  } catch (error) {
+    console.log("Error fetching random image:", error);
+  }
+}
