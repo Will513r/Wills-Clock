695b3968c0f658dd5637cb0cc7e7d967d48be96a
William Ray <will513r@gmail.com>
2024-05-18 09:19:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function updateClock() {
   var now = new Date();
   var formattedTime = now.toLocaleTimeString("en-US", { hour12: true });
-
   document.getElementById("clock").innerHTML = formattedTime;
 }
 
